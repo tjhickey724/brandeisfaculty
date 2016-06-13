6b1327acc8b83cf90800b5869e7c9728f6f1603a
tim <tjhickey@brandeis.edu>
2016-06-13 09:09:40 -0400

    added feature to select faculty from a single department

--- client/facQuiz.js
+++ client/facQuiz.js
@@ -1,22 +1,39 @@
-Session.set('showCompleted',false);	
+Session.set('showCompleted',false);
 Session.set('facnum',322);
 facSize = FacData.find().count();
 
 Template.facQuiz.helpers({
 	randGuess: function() {
-		var z = FacData.findOne({facCount:Session.get('facnum')});
+		const z = Session.get('currFac');
 		return z;
-	}
+	},
+
+	deptFac:function(dept){
+		return(FacData.find({depts:{$elemMatch:{descr:'Computer Science'}}},{sort:{lastname:1}}))},
+
+	depts: ["African and Afro-American Studies", "American Studies", "Anthropology", "Biochemistry", "Biology", "Business", "Chemistry", "Classical Studies", "Comparative Literature", "Computer Science", "East Asian Studies", "Economics", "Education", "English", "Environmental Studies", "Film, Television and Interactive Media", "Fine Arts", "Genetic Counseling", "German, Russian, and Asian Languages and Literature", "Health:  Science, Society, and Policy", "History", "History of Ideas", "Hornstein Program in Jewish Professional Leadership", "International Business School", "International and Global Studies", "Islamic and Middle Eastern Studies", "Italian Studies", "Journalism", "Language and Linguistics", "Latin American and Latino Studies", "Legal Studies", "Mathematics", "Medieval and Renaissance Studies", "Music", "Near Eastern and Judaic Studies", "Peace, Conflict, and Coexistence Studies", "Philosophy", "Physics", "Politics", "Psychology", "Rabb School of Continuing Studies, Division of Graduate Professional Studies", "Religious Studies", "Romance Studies", "Sociology", "South Asian Studies", "The Heller School for Social Policy and Management", "Theater Arts", "Women's, Gender and Sexuality Studies"],
+	
+
+
 })
 
 Template.facQuiz.events({
-	'click #tryQuiz': 
+	'click #tryQuiz':
 		function(){
 			var count = FacData.find().count()
 			var randFac = Math.floor( Math.random() * count );
 			Session.set('facnum',randFac);
 			Session.set('showCompleted',false);
 			console.log("in tryQuiz: facnum -> "+Session.get('facnum'));
+			const dept = $(".js-dept").val();
+			console.dir(dept);
+			var zs = FacData.find(
+				{ depts:{$elemMatch:{descr: dept}}}).fetch();
+			console.dir(zs);
+			const j = _.random(zs.length-1);
+			const z = zs[j];
+			Session.set('facnum',z.facCount);
+			Session.set('currFac',z);
 		},
 	'click #show':
 	function(){
@@ -45,4 +62,3 @@ Template.facGuess.events({
 		console.log("session HC ="+Session.get("showCompleted"));
 	}
 });
-
